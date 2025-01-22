21a52eebe1f8f25be00140b90af1b0e5c46bf14f
Gleb Poroshin <gleb.poroshin@lemanapro.ru>
2025-01-22 15:51:30 +0300

    Implement decompose navigation in  ios

--- composeApp/src/iosMain/kotlin/com/gleb/lemana/task/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/gleb/lemana/task/MainViewController.kt
@@ -1,5 +1,16 @@
 package com.gleb.lemana.task
 
+import RootComponent
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import org.koin.compose.koinInject
+import org.koin.core.parameter.parametersOf
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val lifecycle = LifecycleRegistry()
+    val rootComponent = koinInject<RootComponent> {
+        parametersOf(DefaultComponentContext(lifecycle)) 
+    }
+    App(rootComponent)
+}
